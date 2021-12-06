4938a9f03a002557a41aa5ff8daba3326f279a9b
kjhf <kristian.f@hotmail.co.uk>
2021-12-06 01:06:06 +0000

    just git things

--- src/day_5.py
+++ src/day_5.py
@@ -2,72 +2,102 @@ from typing import List
 
 from src.common import get_input
 
-
-def day_4():
-    input_lines = get_input(day=4)
-
-    # First line are the numbers drawn, and the following are the bingo boards
-    numbers_to_call = [int(x) for x in input_lines[0].split(',')]
-    input_lines = input_lines[1:]
-
-    # Slate's note: Pandas is a good solution for this, but as it's an external dependency
-    # (despite being very well-used and documented), I'll disregard it
-    bingo_boards: List[List[int]] = []
-    size_of_board = 5
-
-    for i in range(0, len(input_lines)):
-        # Join across the new lines
-        bingo_board = ' '.join(input_lines[i * size_of_board:(i+1) * size_of_board])
-        # Split by space to get the numbers (and remove whitespace)
-        bingo_boards.append([int(x) for x in bingo_board.split(' ') if x])
-
-    # Foreach number that's drawn, check the boards.
-    # A winning board has at least one complete row or column of marked numbers.
-    # The score can now be calculated. Sum of all **unmarked** numbers on that board, multiply that by the number that
-    # was just called when the board won to get the final score.
-    drawn_numbers = numbers_to_call[:size_of_board]  # Small optimisation, we cannot win until at least the row size has been called
-    for drawn_number in numbers_to_call[size_of_board:]:
-        drawn_numbers.append(drawn_number)
-
-        for n, bingo_board in enumerate(bingo_boards):
-            if is_winner(bingo_board, drawn_numbers):
-                score = calculate_score(bingo_board, drawn_numbers, drawn_number)
-                print(f"Removing board [{n}/{len(bingo_boards)}] with score {score}")
-                bingo_boards.remove(bingo_board)
-
-    print("All done!")
+from dataclasses import dataclass
 
 
-def is_winner(board: List[int], called_numbers: List[int]):
-    row_size = int(len(board) ** 0.5)
+@dataclass
+class Vector:
+    x_start: int
+    x_end: int
+    y_start: int
+    y_end: int
+
+    @staticmethod
+    def from_str(parse: str):
+        parts = parse.partition("->")
+        start = parts[0].strip().partition(",")
+        end = parts[2].strip().partition(",")
+
+        x_start = int(start[0].strip())
+        x_end = int(end[0].strip())
+        y_start = int(start[2].strip())
+        y_end = int(end[2].strip())
+
+        return Vector(
+            x_start=x_start,
+            x_end=x_end,
+            y_start=y_start,
+            y_end=y_end
+        )
+
+    @property
+    def x_diff(self):
+        return self.x_end - self.x_start
+
+    @property
+    def x_changing(self):
+        return self.x_start != self.x_end
+
+    @property
+    def y_diff(self):
+        return self.y_end - self.y_start
+
+    @property
+    def y_changing(self):
+        return self.y_start != self.y_end
+
+
+def day_5():
+    input_lines = get_input(day=5)
+
+    # x1,y1 -> x2,y2
+    # 1,1 -> 1,3 covers points 1,1, 1,2, and 1,3.
+    # For part 1, consider horizontal and vertical lines (x1 = x2 or y1 = y2)
+    # For part 2,
+    # An entry like 1,1 -> 3,3 covers points 1,1, 2,2, and 3,3.
+    # An entry like 9,7 -> 7,9 covers points 9,7, 8,8, and 7,9.
+    all_vectors = [Vector.from_str(line) for line in input_lines]
+    part_1_vectors = [v for v in all_vectors if (v.x_start == v.x_end) or (v.y_start == v.y_end)]
+    print(f"{len(all_vectors)} vectors loaded, of which we're looking at {len(part_1_vectors)} for part 1. "
+          f"First loaded vector is {all_vectors[0]}, and the last is {all_vectors[-1]}.")
+
+    print("Part 1: ")
+    do_the_thing(part_1_vectors)
+    print("\nPart 2: ")
+    do_the_thing(all_vectors)
+
+
+def do_the_thing(vectors: List[Vector]):
+    # Init the board
+    row_size = 1000
+    board = [0] * (row_size ** 2)
+
+    # Insert all the vectors
+    for v in vectors:
+        for i in range(0, max(abs(v.x_diff), abs(v.y_diff)) + 1):
+            x_i = i if v.x_diff > 0 else -i if v.x_diff < 0 else 0
+            y_i = i if v.y_diff > 0 else -i if v.y_diff < 0 else 0
+            board[get_index(v.x_start + x_i, v.y_start + y_i, row_size)] += 1
+
+    # Score is how many points do at least two lines overlap?
+    score = len([cell for cell in board if cell > 1])
+    print(f"{score=}")
+
+    # Print a board to file because why not
+    dump_board(board, row_size)
+
+
+def dump_board(board: List[int], row_size: int):
+    file_content = []
     for i in range(0, row_size):
-        row = row_ints(i, row_size, board)
-        row_wins = all(num in called_numbers for num in row)
-        column = list(column_ints(i, row_size, board))
-        column_wins = all(num in called_numbers for num in column)
-        if row_wins or column_wins:
-            # print(f"Winner: {row_size=} {row=} {row_wins=} {column=} {column_wins=}")
-            # print(f" Board: {board=}")
-            return True
-    return False
+        file_content.append(' '.join([f"{cell:01}" for cell in board[i * row_size:(i + 1) * row_size]]) + '\n')
+    with open(file="board.txt", mode='w', encoding='utf-8') as the_file:
+        the_file.writelines(file_content)
 
 
-def row_ints(row_index: int, row_size: int, board: List[int]):
-    return board[row_index * row_size: (row_index + 1) * row_size]
-
-
-def column_ints(column_index: int, row_size: int, board: List[int]):
-    for i in range(column_index, row_size**2, row_size):
-        yield board[i]
-
-
-def calculate_score(board: List[int], drawn_numbers: List[int], last_drawn_number: int):
-    # Sum of all **unmarked** numbers on that board, multiply that by the number that
-    # was just called when the board won to get the final score.
-    result = sum([num for num in board if num not in drawn_numbers])
-    # print(f"{result=} * {last_drawn_number} = {result*last_drawn_number}")
-    return result * last_drawn_number
+def get_index(x: int, y: int, row_size: int):
+    return x + (y * row_size)
 
 
 if __name__ == '__main__':
-    day_4()
+    day_5()
